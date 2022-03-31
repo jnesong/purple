f3fcfd08e43f6149982990b0a9f6dde5db1eb74b
Jenny Chau <jnechau@gmail.com>
2022-03-31 13:09:41 -0400

    checking how git commits with tests

--- FindFirstDuplicate.test.js
+++ FindFirstDuplicate.test.js
@@ -3,3 +3,7 @@ const findFirstDuplicate = require('./FindFirstDuplicate')
 test ('empty array returns -1', () => {
     expect(findFirstDuplicate([])).toBe(-1)
 })
+
+test ('single character array returns -1', () => {
+    expect(findFirstDuplicate([2])).toBe(-1)
+})
