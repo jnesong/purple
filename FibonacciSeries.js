73e7517e3d365503c316bf5fd5d84a075eb6b3f4
Jenny Chau <jnechau@gmail.com>
2022-03-31 15:48:58 -0400

    cleaned up fibo

--- FibonacciSeries.js
+++ FibonacciSeries.js
@@ -2,39 +2,36 @@
 //O: the nth element in the Fibonacci series (every value is the sum of the two values preceding it)
 //C: zero-indexing, sequence starts at 0, followed by 1
 //E: n of 0 or n of 1
-//Time complexity:
-// Space complexity: 
+//Time complexity: O(n)
+// Space complexity: O(n)
 
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 
-function fibonacci(n){
-    if (n === 0) {
-        return 0
-    } 
-    else if (n === 1 || n === 2) {
-        return 1
-    } else if (Number.isInteger(n)===false) {
+function fibonacci(n) {
+    if (Number.isInteger(n) === false) {
         return null
     }
     else {
         let fib = [0, 1, 1]
-        for (i = 2; i < n; i++) {
-            let next = fib[i] + fib[i-1]
-            fib.push(next)
+        if (n > 2) {
+            for (i = 2; i < n; i++) {
+                let next = fib[i] + fib[i - 1]
+                fib.push(next)
+            }
         }
         return fib[n]
     }
 
 }
 
-console.log(fibonacci(0)=== 0)
-console.log(fibonacci(1)=== 1)
-console.log(fibonacci(2)=== 1)
-console.log(fibonacci(3)=== 2)
-console.log(fibonacci(10)=== 55)
-console.log(fibonacci("a")=== null)
-console.log(fibonacci("a"))
-console.log(fibonacci("-4"))
+console.log(fibonacci(0) === 0)
+console.log(fibonacci(1) === 1)
+console.log(fibonacci(2) === 1)
+console.log(fibonacci(3) === 2)
+console.log(fibonacci(10) === 55)
+console.log(fibonacci(11) === 89)
+console.log(fibonacci("a") === null)
+console.log(fibonacci("-4") === null)
 
 
 
