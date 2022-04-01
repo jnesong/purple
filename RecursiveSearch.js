ce60e4cc22cd4c08fa316901de0628fe503ed15d
Jenny Chau <jnechau@gmail.com>
2022-04-01 11:24:46 -0400

    included more test cases

--- RecursiveSearch.js
+++ RecursiveSearch.js
@@ -2,8 +2,8 @@
 //O: true, if target is in array; false if not
 //C: use recursion
 //E:
-//Time complexity:
-//Space complexity:
+//Time complexity: O(n)
+//Space complexity: O(1)
 
 function recursiveSearch(array, target) {
     // console.log(array)
@@ -18,4 +18,6 @@ function recursiveSearch(array, target) {
 }
 
 console.log(recursiveSearch([0, 1, 23, 4], 1) === true)
+console.log(recursiveSearch([3, 2, 1], 4) === false)
 console.log(recursiveSearch([0, 1, 23, 4], 2) === false)
+console.log(recursiveSearch([1, 2, 3], 2) === true)
