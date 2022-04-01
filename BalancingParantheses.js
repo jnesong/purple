81b6ee2ae8869a85847813d547dbf91090113480
Jenny Chau <jnechau@gmail.com>
2022-04-01 11:41:02 -0400

    done with balancing parentheses stack solution

---
+++ BalancingParantheses.js
@@ -0,0 +1,33 @@
+//I: string containing only parentheses
+//O: number of additional parentheses needed for the string to be considered balanced
+//C: input string will have a min length of 1
+//E: only left or only right parentheses, start with closing and ends with opening
+//Time complexity:
+//Space complexity:
+
+function balancingParentheses ( string ){
+    let arr = string.split("")
+    let stackL = []
+    let stackR = []
+    for (let i=0; i<arr.length; i++){
+        if (arr[i]==="("){
+            stackL.push(arr[i])
+        } else if ( arr[i]===")" && stackL.length>0){
+            stackL.pop()
+        } else {
+            stackR.push(arr[i])
+        }
+    }
+    return (stackL.length + stackR.length)
+}
+
+
+console.log(balancingParentheses('(()())') === 0);
+console.log(balancingParentheses('()))') === 2);
+console.log(balancingParentheses(')') === 1);
+console.log(balancingParentheses('(((') === 3);
+console.log(balancingParentheses(')((((()))((())))()(()))(') === 2);
+
+
+
+
