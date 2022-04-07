1d9b7bad4c9b1032da286b31217f4b865ca9da03
Jenny Chau <jnechau@gmail.com>
2022-04-06 21:47:37 -0400

    bubble sort with og sort algo

--- BubbleSort.js
+++ BubbleSort.js
@@ -27,3 +27,15 @@ console.log(bubbleSort([1, 2, 3]));
 console.log(bubbleSort([]));
 console.log(bubbleSort([0, 4, 5, 9, 5]));
 
+// function bubbleSort(arr) {
+//     for(let i = arr.length; i > 0; i--) {
+//         for(let j = 0; j < i - 1; j++) {
+//             if(arr[j] > arr[j + 1]) {
+//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//             }
+//         }
+//     }
+
+//     return arr;
+// }
+
