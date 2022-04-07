60970ce68c9d4325b00afdf86bc83e2230d413e9
Jenny Chau <jnechau@gmail.com>
2022-04-06 21:54:54 -0400

    bubble sort with og, recursive, and es6 destructuring

--- BubbleSort.js
+++ BubbleSort.js
@@ -10,32 +10,49 @@ Space Complexity: O(n²)
 function bubbleSort(arr) {
 
     for (let i = 0; i < arr.length; i++) {
-        let j = i + 1
+        let j = i + 1;
         if (arr[j] < arr[i]) {
-            let temp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = temp
-            return bubbleSort(arr)
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            return bubbleSort(arr);
         }
     }
 
-    return arr
-}
+    return arr;
+};
 
 console.log(bubbleSort([3, 2, 1, 4]));
 console.log(bubbleSort([1, 2, 3]));
 console.log(bubbleSort([]));
 console.log(bubbleSort([0, 4, 5, 9, 5]));
 
-// function bubbleSort(arr) {
-//     for(let i = arr.length; i > 0; i--) {
-//         for(let j = 0; j < i - 1; j++) {
-//             if(arr[j] > arr[j + 1]) {
-//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-//             }
-//         }
-//     }
+/*
+ES6 no temp variable, iterate backwards
+function bubbleSort(arr) {
+    for(let i = arr.length; i > 0; i--) {
+        for(let j = 0; j < i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
 
-//     return arr;
-// }
+    return arr;
+}
 
+Original bubble sort
+function bubbleSort(arr) {
+    for (let i=0; i<arr.length; i++){
+        for (let j=0; j<arr.length; j++){
+            if (arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+};
+
+*/
