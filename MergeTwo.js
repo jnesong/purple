e6ff9c13a54e4bba9e0dd1761f9550cf84554b9a
Jenny Chau <jnechau@gmail.com>
2022-04-12 13:54:34 -0400

    merge two attempt

--- MergeTwo.js
+++ MergeTwo.js
@@ -1,5 +1,5 @@
 /* 
-I: head nodes of two linked lists
+I: head nodes of two singly linked lists
 O: head of the merged two linked lists spliced together and sorted
 C: linked lists length is between 0 and 50, the value is between -100 and 100, the lists are already sorted in increasing order
 E: empty linked lists
@@ -7,6 +7,50 @@ Time Complexity:
 Space Complexity: 
 */
 
-function mergeTwo ( listA, listB) {
-    
+function ListNode(value = 0, nextIn = null) {
+    this.val = value
+    this.next = nextIn
 }
+
+const pretendLinkedA = [
+    { value: 1, next: 2 },
+    { value: 2, next: 4 },
+    { value: 4, next: 9 },
+    { value: 9, next: null }
+]
+
+const pretendLinkedB = [
+    { value: 1, next: 3 },
+    { value: 3, next: 7 },
+    { value: 7, next: 12 },
+    { value: 12, next: null }
+]
+
+function mergeTwo(listA, listB) {
+
+    if (listA.length !== 0 && listB.length !== 0) {
+
+        let currentA = listA[0]
+        let currentB = listB[0]
+
+        if (currentA <= currentB) {
+            let x = listA.shift()
+            const result = new ListNode(x, mergeTwo(listA, listB))
+            console.log(result)
+        } else {
+            let y = listB.shift()
+            const result = new ListNode(y, mergeTwo(listA, listB))
+            console.log(result)
+        }
+
+        if (currentA <= currentB) {
+            return currentA
+        } else { return currentB }
+
+    }
+   
+    return null
+
+}
+
+console.log(mergeTwo(pretendLinkedA, pretendLinkedB))
