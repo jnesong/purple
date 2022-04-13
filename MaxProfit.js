7f6db988415cdf2b55cfffaaa6de3a2e6c76add4
Jenny Chau <jnechau@gmail.com>
2022-04-13 16:29:36 -0400

    start max profit

---
+++ MaxProfit.js
@@ -0,0 +1,16 @@
+/* 
+I: array of prices, index represents day
+O: max profit/difference between two prices
+C: must buy before sell, so the smaller price must have an earlier/smaller index
+E: if max profit is 0, return 0
+Time Complexity: 
+Space Complexity: 
+*/
+
+
+// loop through, check if next is 
+
+function maxProfit () {
+
+
+}
--- MergeTwo.js
+++ MergeTwo.js
@@ -3,8 +3,8 @@ I: head nodes of two singly linked lists
 O: head of the merged two linked lists spliced together and sorted
 C: linked lists length is between 0 and 50, the value is between -100 and 100, the lists are already sorted in increasing order
 E: empty linked lists
-Time Complexity: 
-Space Complexity: 
+Time Complexity: O(n)
+Space Complexity: O(1)
 */
 
 class LinkedList {
