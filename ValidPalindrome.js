d177e91d6df3e02a854af85db8403c681ccfc8fb
Jenny Chau <jnechau@gmail.com>
2022-04-14 15:30:53 -0400

    rusing .replace alternative included

--- ValidPalindrome.js
+++ ValidPalindrome.js
@@ -3,18 +3,18 @@ I: a string
 O: true or false
 C: convert all uppercase to lowercase, remove all non-alphanumeric characters (letters and numbers), ASCII characters
 E: single character, empty string
-Time Complexity: 
-Space Complexity: 
+Time Complexity: O(n)
+Space Complexity: O(1)
 */
 
-function isPalindrome ( s ) {
-    for (let [i, j]=[0, s.length-1]; i<j;){
-        if(/\W|_/.test(s[i])){
+function isPalindrome(s) {
+    for (let [i, j] = [0, s.length - 1]; i < j;) {
+        if (/\W|_/.test(s[i])) {
             i++
-        } else if (/\W|_/.test(s[j])){
+        } else if (/\W|_/.test(s[j])) {
             j--
         } else {
-            if (s[i].toLowerCase()!==s[j].toLowerCase()){
+            if (s[i].toLowerCase() !== s[j].toLowerCase()) {
                 return false
             }
             i++
@@ -23,4 +23,13 @@ function isPalindrome ( s ) {
     } return true
 }
 
+// function isPalindrome() {
+//     s = s.toLowerCase().replace(/\W|_/gi, '');
+//     for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+//         if (s.charAt(i) !== s.charAt(j)) return false;
+//     }
+//     return true;
+// }
+
 console.log(isPalindrome("RA_ce car"))
+
