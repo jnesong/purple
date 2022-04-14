32316ed4fe595bea5ad3ac536cbd360f5ccf8e59
Jenny Chau <jnechau@gmail.com>
2022-04-14 18:13:38 -0400

    charCodeAt() method with frequency array

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -7,29 +7,50 @@ Time Complexity: O(n)
 Space Complexity: O(n)
 */
 
-function isAnagram( s, t ) {
-    if (s===t) {return true}
-    if(s.length!==t.length){return false}
-    let map = {}
-    for (let i=0; i<s.length; i++){
-        if (map[s[i]]){
-            map[s[i]]++
-        } else {
-            map[s[i]] = 1
-        }
-    }
+// function isAnagram( s, t ) {
+//     if (s===t) {return true}
+//     if(s.length!==t.length){return false}
+//     let map = {}
+//     for (let i=0; i<s.length; i++){
+//         if (map[s[i]]){
+//             map[s[i]]++
+//         } else {
+//             map[s[i]] = 1
+//         }
+//     }
     
-    for (let j=0; j<t.length; j++){
-        if (!map[t[j]]) {
-            return false
-        } else {
-            map[t[j]]--
+//     for (let j=0; j<t.length; j++){
+//         if (!map[t[j]]) {
+//             return false
+//         } else {
+//             map[t[j]]--
+//         }
+//     }
+
+//     console.log(map)
+
+//     return true
+// }
+
+function isAnagram( s, t ) {
+    let f1 = [];
+    let f2 = [];
+    for (let i = 0; i < 26; i++) {
+        f1[i] = 0;
+        f2[i] = 0;
+    }
+    for (let i = 0; i < s.length; i++) {
+        f1[s.charCodeAt(i) - 97]++;
+    }
+    for (let i = 0; i < t.length; i++) {
+        f2[t.charCodeAt(i) - 97]++;
+    }
+    for (let i = 0; i < 26; i++) {
+        if (f1[i] !== f2[i]) {
+            return false;
         }
     }
-
-    console.log(map)
-
-    return true
+    return true;
 }
 
 console.log(isAnagram("a", "a")===true)
