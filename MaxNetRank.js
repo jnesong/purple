05267162d559f5f83d9934f270a3a8a903833c5c
Jenny Chau <jnechau@gmail.com>
2022-04-25 13:04:03 -0400

    max net rank attempt

--- MaxNetRank.js
+++ MaxNetRank.js
@@ -9,45 +9,41 @@ Space:
 
  */
 
-function maxNetRank (n, roads) {
-    let map ={}
-    for (let r=0; r<roads.length; r++){
+function maxNetRank(n, roads) {
+    let map = {}
+    for (let r = 0; r < roads.length; r++) {
         map[roads[r]] = roads[r]
-        if(map[roads[r][0]]) {
+        if (map[roads[r][0]]) {
             map[roads[r][0]]++
-        } else { 
-            map[roads[r][0]]=1
+        } else {
+            map[roads[r][0]] = 1
         }
-        if(map[roads[r][1]]) {
+        if (map[roads[r][1]]) {
             map[roads[r][1]]++
-        } else { 
-            map[roads[r][1]]=1
+        } else {
+            map[roads[r][1]] = 1
         }
     }
+    
+    let max = map[0]
+    let maxIndex = 0
+    let secondMax = map[1]
+    let secondMaxIndex = 1
+    for (let i = 1; i < n; i++) {
 
-    console.log(map)
-        
-        let max = map[0]
-        let maxIndex = 0
-        let secondMax = map[1]
-        let secondMaxIndex= 1
-        for (let i=0;i<n;i++){
-
-            if (map[i]>max){
-                secondMax = max
-                secondMaxIndex = maxIndex
-                max = map[i]
-                maxIndex = i
+        if (map[i] >= max) {
+            secondMax = max
+            secondMaxIndex = maxIndex
+            max = map[i]
+            maxIndex = i
+            if (!(map[[maxIndex, secondMaxIndex]] || map[secondMaxIndex, maxIndex])) {
+                max++
             }
-            
         }
-    
 
-        if (map[[maxIndex, secondMaxIndex]] || map[[secondMaxIndex, maxIndex]]){
-            return max+secondMax-1
-        } else { return max+secondMax}   
-
-    
+    }
+    return max + secondMax - 1
 };
 
-console.log(maxNetRank(5, [[0,1],[0,3],[1,2],[1,3],[2,3],[2,4]]))
+console.log(maxNetRank(5, [[0, 1], [0, 3], [1, 2], [1, 3], [2, 3], [2, 4]]))
+console.log(maxNetRank(4, [[0,1],[0,3],[1,2],[1,3]]))
