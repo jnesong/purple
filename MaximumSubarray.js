684ff301f0b6e603b656465d44a5073b7cb34568
Jenny Chau <jnechau@gmail.com>
2022-04-29 14:56:02 -0400

    changes to max subArray

--- MaximumSubarray.js
+++ MaximumSubarray.js
@@ -9,15 +9,14 @@ Space Complexity: O(n)
 */
 
 function maxSubArray(nums) {
-    let max = nums[0]
-    let current = 0
+  let currentSum = 0
+  let max = nums[0]
 
-    for (let i = 0; i < nums.length; i++) {
-      if (current<0){ current = 0}
-      current += nums[i]
-      max = Math.max(max, current)
-    }
-    return max
+  for (let i = 0; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum += nums[i])
+    max = Math.max(max, currentSum)
+  }
+  return max
 
 }
 
