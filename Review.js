19bba1421b1a2b29dafd6a93742eb323869f0a17
Jenny Chau <jnechau@gmail.com>
2022-05-03 14:52:39 -0400

    fibonacci review

--- Review.js
+++ Review.js
@@ -38,4 +38,30 @@ if there are no dupes, the for loop will end and -1 will be returned to indicate
 // console.log(findFirstDuplicate([4,6,9,1,3,6]))
 // console.log(findFirstDuplicate([]))
 
+const fibonacci = (num) => {
+    let myFibonacci = [0, 1]
+    for (let i=2; i<num+1; i++) {
+        let x = myFibonacci.length-1
+        let y = myFibonacci.length-2
+        let z = myFibonacci[x] + myFibonacci[y]
+        myFibonacci.push(z)
+    }
+
+    let w = myFibonacci.length-1
+    return myFibonacci[w]
+}
+
+/*
+fibonacci() takes in a number and returns that number element in the fibonacci sequence, a sequence where each element is the sum of the two elements before it
+first declare an array with the first two elements in the fibonacci sequence, 0 and 1
+then begin constructing the sequence by starting at i=2, since we already have the first two elements, up until the num given
+we construct one element at a time by pushing the sum of the last two elements in the fibonacci onto the same fibonacci array
+finally, we return the last element in the array which will be the position num
+because javascript arrays start at index 0, this would be the array.length-1
+I like this method because it clearly/simply also works for edge cases where the input is 0 or 1
+*/
+
+// console.log(fibonacci(10))
+// console.log(fibonacci(0))
+
 
