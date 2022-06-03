9107c1472b21db54fd85d533d68925678f4018d6
Jenny Chau <jnechau@gmail.com>
2022-06-03 16:10:30 -0400

    balanced binary in process

--- BalancedBinary.js
+++ BalancedBinary.js
@@ -1,6 +1,6 @@
 /* 
 I: root
-O: true or false
+O: boolean
 C: number of nodes 0-5000, node value is between -10000 and 10000
 E: 
 Time Complexity: 
@@ -12,39 +12,22 @@ Space Complexity:
  *     this.right = (right===undefined ? null : right)
  * }
  * 
-for each node, loop through left, add left nodes to stack then loop through right and pop off
-cannot be more than 1 node difference
+
 */
 
 
 function isBalanced(root) {
-
-    if (!root) return true;
-    let count = 0
-
-    leftMinus(root);
-    rightAdd(root);
-
-    function leftMinus(root){
-        console.log(root, count)
-        if (!root) {return console.log("end")}
-        count--
-        leftMinus(root.left);
-    }
-    function rightAdd(root){
-        console.log(root, count)
-        if (!root) {return console.log("end")}
-        count++
-        rightAdd(root.right);
-    }
+    if (!root) return true; // current node is not-true (null)), return true
     
-    
-    if (count>=-2 && count<=2) {
-        return true
-    } else {
-        return false
-    }
-}
+    if (Math.abs( getHeight(root.left)-getHeight(root.right) )>1) return false;
 
-console.log(isBalanced([1,2,3,4,5,6,null,8]))
-// console.log(isBalanced([3, 9, 20, null, null, 15, 7]))
+    return isBalanced(root.left) && isBalanced(root.right);
+};
+
+function getHeight(root) {
+    if(!root) return 0; //current node is not-true (null), return 0
+    let left = getHeight(root.left);
+    let right = getHeight(root.right);
+    return Math.max(left, right) +1;
+};
+
--- LinkedListCycle.js
+++ LinkedListCycle.js
@@ -9,13 +9,12 @@ Space Complexity: O(n)
 
 const hasCycle = (head) => {
     let map = new Map();
-    let current = head
+    let current = head;
     while (current !==null) {
         map.set(current, 1)
     if (map.has(current.next)) return true
     else if (current.next===null) return false
     else current = current.next
-}
-    
-    return false
+};
+    return false;
 };
